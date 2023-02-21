6ac37740eecdfaaf3a62fd5eb6847b16d8c3e0f5
Ashik Mahmood <wd.ashik04@gmail.com>
2023-02-22 03:27:25 +0600

    header and home page pertially added

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <>
+            <Navbar className="border-bottom" bg="white" variant="white" sticky="top" collapseOnSelect expand="lg">
+                <Container className="gap-5 ">
+                    <Navbar.Brand as={Link} to="/home#home"><h1 style={{fontWeight:"bold"}}>Dentist</h1></Navbar.Brand>
+
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-start text-dark gap-5 ">
+                        <Nav.Link as={Link} to="/home#home" ><span className="size-style">Home</span> </Nav.Link>
+
+                        <Nav.Link  as={Link} to="/home#about" ><span className="size-style">About</span></Nav.Link>
+
+                        <Nav.Link as={Link} to="/home#cart" ><span className="size-style">Cart</span></Nav.Link>
+
+                        <Nav.Link as={Link} to="/home#contact" ><span className="size-style">Contact</span></Nav.Link>
+                        
+                       
+                        
+                    </Navbar.Collapse>
+                    
+                    <Nav.Link as={Link} to="/home#contact" ><span className="button-style ">Appointment</span></Nav.Link>
+
+
+                    <Nav.Link as={Link} to="/login" ><span className="button-style">Login</span></Nav.Link>
+
+                    <Nav.Link as={Link} to="/login" ><span className="nav-button">Register</span></Nav.Link>
+                        
+
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import img from '../../media/images/full-equiped-medical-cabinet.jpg';
+import './Home.css';
+const Home = () => {
+    return (
+        <>
+            <div className="home-container">
+                <div className="left-section">
+                    <h1><span style={{color:"rgb(67, 8, 116)"}}>A</span> <span style={{color:"rgb(107, 36, 238)"}}>dentistry</span></h1>
+                    <h1><span style={{color:"rgb(107, 36, 238)"}}>clinic</span> <span style={{color:"rgb(67, 8, 116)"}}>you can</span> </h1>
+                    <h1><span style={{color:"rgb(67, 8, 116)"}}>trust</span></h1>
+                    <p className="text-container">Consult with our dentists and checkup your tooth today to make a healthy smile!</p>
+                    <button className="mx-5 book-button">Book an Appointment</button>
+                    <button className="browse-button">Browse Service</button>
+                </div>
+
+                <div className="separate-width">
+                    <img src={img} alt="img" />
+                </div>
+            </div>
+
+            <div>
+                <h4 className="mt-5 mx-5" style={{color:"rgb(67, 8, 116)"}}>Our Services</h4>
+                <hr className="hr-style"/>
+
+
+            </div>
+        </>
+    );
+};
+
+export default Home;
